a71a627c4eaacba1e63b411682a466d95620fc33
Benjamin-Dekan <146146637+Benjamin-Dekan@users.noreply.github.com>
2026-07-16 18:26:53 -0400

    Implement useState functionality in SearchBar.tsx

--- app/ui/SearchBar.tsx
+++ app/ui/SearchBar.tsx
@@ -1,7 +1,18 @@
+"use client";
+
 import React from "react";
 import Image from "next/image";
+import { ChangeEvent, useState } from "react";
 
 const SearchBar = () => {
+  const [value, setValue] = useState("Enter search...");
+
+  const searchHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    const { target } = event;
+    setValue(target.value);
+    console.log(value);
+  };
+
   return (
     <div className="group flex items-center w-64 rounded-full bg-[#232323] px-4 h-10 text-white gap-2">
       <Image
@@ -15,6 +26,7 @@ const SearchBar = () => {
         type="text"
         placeholder="Search..."
         className="bg-transparent focus:outline-none w-full text-md placeholder-gray-500"
+        onChange={searchHandler}
       />
     </div>
   );
