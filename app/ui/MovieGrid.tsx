a654905ad09dc068bd47d6e979b4702d9432699d
Benjamin-Dekan <146146637+Benjamin-Dekan@users.noreply.github.com>
2026-07-17 14:58:48 -0400

    Add intersection observer skeleton to MovieGrid

--- app/ui/MovieGrid.tsx
+++ app/ui/MovieGrid.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React from "react";
 import MovieCard from "./MovieCard";
+import { useRef, useEffect } from "react";
 
 interface Movie {
   id: number;
@@ -9,6 +10,35 @@ interface Movie {
 }
 
 const DiscoverGrid = ({ data }) => {
+  const callback = (
+    entries: IntersectionObserverEntry[],
+    observer: IntersectionObserver,
+  ) => {
+    if (entries[0].isIntersecting) {
+      console.log("Visible Test");
+    }
+  };
+
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    const options = {
+      root: null,
+      rootMargin: "0px",
+      scrollMargin: "0px",
+      threshold: 1.0,
+    };
+
+    const observer = new IntersectionObserver(callback, options);
+    if (inputRef.current) {
+      observer.observe(inputRef.current);
+    }
+
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
+
   return (
     <div>
       <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-6">
@@ -16,6 +46,7 @@ const DiscoverGrid = ({ data }) => {
           <MovieCard key={movie.id} movie={movie} />
         ))}
       </div>
+      <div ref={inputRef}></div>
     </div>
   );
 };
