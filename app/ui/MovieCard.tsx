a884cc163888873fad10e36ed3bd1677c93bdaa9
Benjamin-Dekan <146146637+Benjamin-Dekan@users.noreply.github.com>
2026-07-18 19:00:10 -0400

    Fix aspect ratio class syntax

--- app/ui/MovieCard.tsx
+++ app/ui/MovieCard.tsx
@@ -17,7 +17,7 @@ const MovieCard = ({ movie, className = "w-full" }: MovieProps) => {
   return (
     <Link href={`/movie/${movie.id}`} className={`block ${className}`}>
       <div className="bg-[#343434] ml-1 rounded-3xl shadow-md overflow-hidden transition-all duration-300 ease-out hover:-translate-y-2 hover:shadow-2xl hover:shadow-[#7a7a7a]/30">
-        <div className="relative w-full aspect-[2/3]">
+        <div className="relative w-full aspect-2/3">
           <Image
             src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
             alt={movie.title}
