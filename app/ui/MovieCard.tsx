be4d2fe3ebe425322de9a309c7586d6567c9dbe6
Benjamin-Dekan <146146637+Benjamin-Dekan@users.noreply.github.com>
2026-07-18 18:51:25 -0400

    Fix sizing issues of posters to be identical

--- app/ui/MovieCard.tsx
+++ app/ui/MovieCard.tsx
@@ -17,17 +17,14 @@ const MovieCard = ({ movie, className = "w-full" }: MovieProps) => {
   return (
     <Link href={`/movie/${movie.id}`} className={`block ${className}`}>
       <div className="bg-[#343434] ml-1 rounded-3xl shadow-md overflow-hidden transition-all duration-300 ease-out hover:-translate-y-2 hover:shadow-2xl hover:shadow-[#7a7a7a]/30">
-        <Image
-          src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-          alt={movie.title}
-          width={500}
-          height={750}
-          className="w-full h-auto object-cover"
-        />
-
-        {/* <h3 className="text-center text-white py-3 px-2 font-semibold tracking-wide text-sm md:text-base truncate">
-          {movie.title}
-        </h3> */}
+        <div className="relative w-full aspect-[2/3]">
+          <Image
+            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+            alt={movie.title}
+            fill
+            className="object-cover"
+          />
+        </div>
       </div>
     </Link>
   );
