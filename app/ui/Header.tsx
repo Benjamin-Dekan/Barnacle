10115bbaf455d3e89e2d5870d643ee46a16cef0f
Benjamin-Dekan <146146637+Benjamin-Dekan@users.noreply.github.com>
2026-07-20 15:19:09 -0400

    Add discover page shortcut in icon

--- app/ui/Header.tsx
+++ app/ui/Header.tsx
@@ -4,6 +4,7 @@ import NavLinks from "./NavLinks";
 import SearchBar from "./SearchBar";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
+import Link from "next/link";
 
 const Header = () => {
   const router = useRouter();
@@ -17,9 +18,9 @@ const Header = () => {
     <header className="sticky z-50 top-0 w-full bg-[#111111]/80 border-b border-white/10 backdrop-blur-md">
       <div className="flex justify-between items-center h-19 px-8 relative">
         <div className="flex items-center gap-8">
-          <div className="relative w-14 h-14">
+          <Link href="/discover" className="relative w-14 h-14">
             <Image src="/barnacle.svg" fill alt="Picture of barnacle logo" />
-          </div>
+          </Link>
           <div className="text-3xl -ml-4 mr-134">Barnacle</div>
         </div>
 
