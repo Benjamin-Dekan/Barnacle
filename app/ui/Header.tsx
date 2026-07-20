431051c61d7e1566b2de4b337639e4270e6e8c95
Benjamin-Dekan <146146637+Benjamin-Dekan@users.noreply.github.com>
2026-07-19 20:04:59 -0400

    Add barnacle images/icons

--- app/ui/Header.tsx
+++ app/ui/Header.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import NavLinks from "./NavLinks";
 import SearchBar from "./SearchBar";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
 
 const Header = () => {
   const router = useRouter();
@@ -14,13 +15,20 @@ const Header = () => {
 
   return (
     <header className="sticky z-50 top-0 w-full bg-[#111111]/80 border-b border-white/5 backdrop-blur-md">
-      <div className="flex justify-between items-center h-16 px-8">
+      <div className="flex justify-between items-center h-16 px-8 relative">
         <div className="flex items-center gap-8">
-          <div>Logo</div>
+          <div className="relative w-14 h-14">
+            <Image src="/barnacle.svg" fill alt="Picture of barnacle logo" />
+          </div>
+          <div className="text-3xl -ml-4 mr-134">Barnacle</div>
+        </div>
+
+        <div className="hidden md:flex absolute left-1/2 -translate-x-1/2 items-center gap-8">
           <NavLinks navLink="/profile" navTitle="Profile" />
           <NavLinks navLink="/watchlist" navTitle="Watchlist" />
           <NavLinks navLink="/discover" navTitle="Discover" />
         </div>
+
         <SearchBar onSearch={searchRouter} />
       </div>
     </header>
