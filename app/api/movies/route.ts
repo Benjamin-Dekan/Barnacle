05ebf6e0f5eddf0822dc8816cd9c7a43a3185961
Benjamin-Dekan <146146637+Benjamin-Dekan@users.noreply.github.com>
2026-07-22 18:22:45 -0400

    Update API endpoint to include provider parameter

--- app/api/movies/route.ts
+++ app/api/movies/route.ts
@@ -4,11 +4,16 @@ export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams;
   const query = searchParams.get("q");
   const page = searchParams.get("page") || 2;
+  const provider = searchParams.get("provider");
 
-  const endpoint = query
-    ? `https://api.themoviedb.org/3/search/movie?api_key=${process.env.TMDB_API_KEY}&query=${query}&page=${page}`
-    : `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.TMDB_API_KEY}&page=${page}&sort_by=popularity.desc`;
-
+  let endpoint;
+  if (query && provider) {
+    endpoint = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.TMDB_API_KEY}&query=${query}&provider=${provider}`;
+  } else if (query) {
+    endpoint = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.TMDB_API_KEY}&query=${query}`;
+  } else {
+    endpoint = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.TMDB_API_KEY}`;
+  }
   const response = await fetch(endpoint);
   const data = await response.json();
 
