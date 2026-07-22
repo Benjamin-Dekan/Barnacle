180b357d92915dfa97e4fd9867d5b5d3585fa755
Benjamin-Dekan <146146637+Benjamin-Dekan@users.noreply.github.com>
2026-07-22 18:47:41 -0400

    Fix encounter two children with same key warning

--- app/api/movies/route.ts
+++ app/api/movies/route.ts
@@ -3,16 +3,16 @@ import { NextRequest, NextResponse } from "next/server";
 export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams;
   const query = searchParams.get("q");
-  const page = searchParams.get("page") || 2;
+  const page = searchParams.get("page") || "1";
   const provider = searchParams.get("provider");
 
   let endpoint;
   if (query && provider) {
-    endpoint = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.TMDB_API_KEY}&query=${query}&provider=${provider}`;
+    endpoint = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.TMDB_API_KEY}&query=${query}&provider=${provider}&page=${page}`;
   } else if (query) {
-    endpoint = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.TMDB_API_KEY}&query=${query}`;
+    endpoint = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.TMDB_API_KEY}&query=${query}&page=${page}`;
   } else {
-    endpoint = `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.TMDB_API_KEY}`;
+    endpoint = `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.TMDB_API_KEY}&page=${page}`;
   }
   const response = await fetch(endpoint);
   const data = await response.json();
