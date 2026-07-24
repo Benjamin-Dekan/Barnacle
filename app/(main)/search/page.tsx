6cf401b481bd4f101f4e42f708a295cd89f874b9
Benjamin-Dekan <146146637+Benjamin-Dekan@users.noreply.github.com>
2026-07-24 18:07:04 -0400

    Add full /search route functionality

--- app/(main)/search/page.tsx
+++ app/(main)/search/page.tsx
@@ -1,5 +1,4 @@
 import DiscoverGrid from "@/app/ui/DiscoverGrid";
-import FilterBar from "@/app/ui/FilterBar";
 
 export default async function SearchPage({
   searchParams,
@@ -7,28 +6,31 @@ export default async function SearchPage({
   searchParams: { q: string; provider: string };
 }) {
   const params = await searchParams;
-  const query = params.q;
-  let endpoint;
+  const query = params.q ?? null;
 
+  let data = { results: [] };
   if (query) {
-    endpoint = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.TMDB_API_KEY}&query=${query}`;
-  } else {
-    endpoint = `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.TMDB_API_KEY}`;
+    const endpoint = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.TMDB_API_KEY}&query=${query}`;
+    const response = await fetch(endpoint);
+    data = await response.json();
   }
-
-  const response = await fetch(endpoint);
-  const data = await response.json();
 
   return (
     <main className="px-8 max-w-[1600px] mx-auto min-h-100">
       <div className="flex gap-10 items-start">
-        <div className="flex-1 min-w-0">
-          <DiscoverGrid
-            data={data.results}
-            query={query}
-            key={`${query ?? ""}`}
-          />
-        </div>
+        {query ? (
+          <div className="flex-1 min-w-0">
+            <DiscoverGrid
+              data={data.results}
+              query={query}
+              key={`${query ?? ""}`}
+            />
+          </div>
+        ) : (
+          <div className=" text-white/40 py-20 min-w-0 flex-1 text-center">
+            There are no movies that matched your query.
+          </div>
+        )}
       </div>
     </main>
   );
