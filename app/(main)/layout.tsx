0e7131368d7859a7d6f2c8d7f2395b493c6a9d76
Benjamin-Dekan <146146637+Benjamin-Dekan@users.noreply.github.com>
2026-07-24 22:36:56 -0400

    Update main layout w/ suspense around header to fix error

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,4 +1,5 @@
 import Header from "../ui/Header";
+import React, { Suspense } from "react";
 
 export default function MainLayout({
   children,
@@ -7,7 +8,9 @@ export default function MainLayout({
 }) {
   return (
     <div className="min-h-screen bg-[#111111]">
-      <Header />
+      <Suspense fallback={null}>
+        <Header />
+      </Suspense>
       <main className="flex-1 overflow-y-auto pt-8 pr-8 pb-8 pl-8 text-white">
         {children}
       </main>
