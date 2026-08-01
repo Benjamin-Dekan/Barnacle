7f06c32758263e822747835b3edac497a147abe7
Benjamin-Dekan <146146637+Benjamin-Dekan@users.noreply.github.com>
2026-07-31 23:15:50 -0400

    feat: complete navbar with logo and enter-site link

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,35 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
       {/* Navigation Bar */}
-      <header></header>
+      <header className="p-6">
+        <div className="flex items-center justify-between max-w-[1600px] mx-auto">
+          <div className="flex items-center gap-3">
+            <Image
+              src="/barnacle.svg"
+              width={52}
+              height={52}
+              alt="Picture of barnacle logo"
+            />
+            <h1 className="text-3xl font-medium">Barnacle</h1>
+          </div>
+
+          <Link
+            href="/discover"
+            className="rounded-full px-4 py-2 bg-black/50 backdrop-blur-md ring-1 ring-white/10 hover:bg-black/70 transition-colors active:scale-95"
+          >
+            Enter site
+          </Link>
+        </div>
+      </header>
 
       {/* Hero */}
       <section></section>
 
-      {/* Features */}
+      {/* Hero */}
       <section></section>
 
       {/* Footer */}
